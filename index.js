71d178f1d77aaad112c3b0dca4fc81d165530a67
Mariel Westervelt <elle.westervelt@gmail.com>
2018-09-30 16:13:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  return kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function appendKitten(name) {
+  
+}
+
+function prependKitten(name) {
+  
+}
